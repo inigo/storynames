1a95f4ffc31b61d4361ee4a13c562321fdc30c75
Inigo <inigo.surguy@gmail.com>
2012-06-07 21:57:27 +0100

    Buttons to create male and female names

--- src/main/scala/net/surguy/storynames/android/MainActivity.scala
+++ src/main/scala/net/surguy/storynames/android/MainActivity.scala
@@ -2,17 +2,33 @@ package net.surguy.storynames.android
 
 import android.app.Activity
 import android.os.Bundle
+import net.surguy.storynames.names.Cultures
+import android.widget.{Button, TextView}
+import android.view.View
 
 /**
  * @todo Add some documentation!
  *
  * @author Inigo Surguy
- * @created 06/06/2012 08:48
  */
-
 class MainActivity extends Activity {
-  override def onCreate(savedInstanceState: Bundle): Unit = {
+  override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
+
+    val text = findViewById(R.id.text).asInstanceOf[TextView]
+    text.setText( Cultures.Roman.maleName() )
+
+    val maleButton = findViewById(R.id.male).asInstanceOf[Button]
+    val femaleButton = findViewById(R.id.female).asInstanceOf[Button]
+
+    maleButton.setOnClickListener( new ClickListener( () => text.setText( Cultures.Roman.maleName() ) ) )
+    femaleButton.setOnClickListener( new ClickListener( () => text.setText( Cultures.Roman.femaleName() ) ) )
+
   }
+
+  class ClickListener(val fn: () => Unit ) extends View.OnClickListener {
+    def onClick(v: View) { fn() }
+  }
+
 }
