b989fcc62a5d715040e0bc6ec573a26651c0802c
Inigo <inigo.surguy@gmail.com>
2012-06-07 21:13:31 +0100

    Move implicits to a separate class

--- src/main/scala/net/surguy/storynames/names/Generator.scala
+++ src/main/scala/net/surguy/storynames/names/Generator.scala
@@ -27,75 +27,6 @@ class Generator {
   private def allUppercase(s:String) = ! s.exists( _.isLower )
 }
 
-object Cultures {
-  private val generator = new Generator()
-
-  implicit def wrapString(first: String) = new {
-    def then(suffix: String):List[Ruleset.Rule] = List(take(first), take(suffix))
-  }
-  implicit def wrapString(first: Ruleset.Rule) = new {
-    def then(suffix: String):List[Ruleset.Rule] = List(first, take(suffix))
-    def then(suffix: Ruleset.Rule):List[Ruleset.Rule] = List(first, suffix)
-  }
-  implicit def wrapStringList(first: List[Ruleset.Rule]) = new {
-    def then(suffix: String):List[Ruleset.Rule] = first ++ List(take(suffix))
-    def then(suffix: Ruleset.Rule):List[Ruleset.Rule] = first ++ List(suffix)
-  }
-
-  implicit def stringToRule(str: String):Ruleset.Rule = take(str)
-  implicit def strToRuleList(str: String):List[Ruleset.Rule] = List(take(str))
-  implicit def ruleToRuleList(rule: Ruleset.Rule):List[Ruleset.Rule] = List(rule)
-
-  val Victorian = generator.readCulture("/baker_street.txt",
-    Map("male" -> Ruleset("MALE" then "SURNAME"),
-      "female" -> Ruleset("FEMALE" then "SURNAME")  ))
-
-  val Celtic = generator.readCulture("/celtic.txt",
-    Map("male" -> Ruleset("MALE"), "female" -> Ruleset("FEMALE")  ))
-
-  val Elizabethan = generator.readCulture("/elizabethan.txt",
-    Map("male" -> Ruleset("MALE" then "SURNAME"), "female" -> Ruleset("FEMALE" then "SURNAME") ))
-
-  val Roman = generator.readCulture("/roman.txt",
-    Map("male" -> Ruleset("PRAENOMEN LIST" then "NOMEN LIST" then "COGNOMEN LIST"),
-      "female" -> Ruleset( feminize("NOMEN LIST") then optional("COGNOMEN LIST")) ))
-
-  val Egyptian = generator.readCulture("/fantasy_egyptian.txt",
-    Map("male" -> Ruleset("ANCIENT MALE"),
-      "female" -> Ruleset("ANCIENT FEMALE"),
-      "god" -> Ruleset("GODS"),
-      "goddess" -> Ruleset("GODDESSES")
-    ))
-
-  val Angels = generator.readCulture("/angels_and_demons.txt",
-    Map("male" -> Ruleset(suffix("MASCULINE NAMES",",") then "TITLES OF ANGELS"),
-      "female" -> Ruleset(suffix("FEMININE NAMES",",") then "TITLES OF ANGELS")
-    ))
-
-  val Russian = generator.readCulture("/russian.txt",
-    Map("male" -> Ruleset(removeBrackets("MALE") then patronymic(removeBrackets("MALE"),"ovich") then "SURNAMES"),
-      "female" -> Ruleset(removeBrackets("FEMALE") then patronymic(removeBrackets("MALE"),"ovna")
-                    then regex("SURNAMES","sky$", "skaya"))
-    ))
-
-  val Biblical = generator.readCulture("/biblical.txt",
-    Map("male" -> Ruleset(oneOf("COMMON MALE", "LESS COMMON MALE", "UNUSUAL MALE") ),
-      "female" -> Ruleset(oneOf("COMMON FEMALE", "LESS COMMON FEMALE", "UNUSUAL FEMALE") )
-    ))
-
-  def take(listName: String) = (c: Culture, random: Random) => c.nameComponent(listName, random)
-  def feminize(rule: Ruleset.Rule) = (c: Culture, random: Random) => rule(c, random).replaceAll("ius$","ia")
-  def optional(rule: Ruleset.Rule, percent: Float = 0.5F) =
-    (c: Culture, random: Random) => if (random.nextFloat() < percent) rule(c, random) else ""
-  def suffix(rule: Ruleset.Rule, suffix: String) =
-    (c: Culture, random: Random) => rule(c, random)+suffix
-  def removeBrackets(rule: Ruleset.Rule) = regex(rule, "\\s*\\(.*\\)")
-  def regex(rule: Ruleset.Rule, regex: String, replacement: String = "") =
-    (c: Culture, random: Random) => rule(c, random).replaceAll(regex, replacement)
-  def patronymic(rule: Ruleset.Rule, text: String) = (c: Culture, random: Random) => rule(c, random).trim+text
-  def oneOf(rules: Ruleset.Rule*) = (c: Culture, random: Random) => rules(random.nextInt(rules.length)).apply(c, random)
-}
-
 case class Culture(name: String, private val lists: Iterable[Names], private val rules: Map[String, Ruleset]) {
   private val rnd = new Random()
 
@@ -115,4 +46,81 @@ case class Ruleset(actions: List[Ruleset.Rule])
 
 object Ruleset {
   type Rule = (Culture, Random) => String
+
+  def take(listName: String) = (c: Culture, random: Random) => c.nameComponent(listName, random)
+  def feminize(rule: Ruleset.Rule) = (c: Culture, random: Random) => rule(c, random).replaceAll("ius$","ia")
+  def optional(rule: Ruleset.Rule, percent: Float = 0.5F) =
+    (c: Culture, random: Random) => if (random.nextFloat() < percent) rule(c, random) else ""
+  def suffix(rule: Ruleset.Rule, suffix: String) =
+    (c: Culture, random: Random) => rule(c, random)+suffix
+  def removeBrackets(rule: Ruleset.Rule) = regex(rule, "\\s*\\(.*\\)")
+  def regex(rule: Ruleset.Rule, regex: String, replacement: String = "") =
+    (c: Culture, random: Random) => rule(c, random).replaceAll(regex, replacement)
+  def patronymic(rule: Ruleset.Rule, text: String) = (c: Culture, random: Random) => rule(c, random).trim+text
+  def oneOf(rules: Ruleset.Rule*) = (c: Culture, random: Random) => rules(random.nextInt(rules.length)).apply(c, random)
+}
+
+object RuleHelpers {
+  import Ruleset._
+
+  implicit def wrapString(first: String) = new {
+    def then(suffix: String):List[Ruleset.Rule] = List(take(first), take(suffix))
+  }
+  implicit def wrapString(first: Ruleset.Rule) = new {
+    def then(suffix: String):List[Ruleset.Rule] = List(first, take(suffix))
+    def then(suffix: Ruleset.Rule):List[Ruleset.Rule] = List(first, suffix)
+  }
+  implicit def wrapStringList(first: List[Ruleset.Rule]) = new {
+    def then(suffix: String):List[Ruleset.Rule] = first ++ List(take(suffix))
+    def then(suffix: Ruleset.Rule):List[Ruleset.Rule] = first ++ List(suffix)
+  }
+
+  implicit def stringToRule(str: String):Ruleset.Rule = take(str)
+  implicit def strToRuleList(str: String):List[Ruleset.Rule] = List(take(str))
+  implicit def ruleToRuleList(rule: Ruleset.Rule):List[Ruleset.Rule] = List(rule)
+}
+
+object Cultures {
+  import RuleHelpers._
+  import Ruleset._
+
+  private val generator = new Generator()
+
+  val Victorian = generator.readCulture("/baker_street.txt",
+    Map("male" -> Ruleset("MALE" then "SURNAME"),
+      "female" -> Ruleset("FEMALE" then "SURNAME")  ))
+
+  val Celtic = generator.readCulture("/celtic.txt",
+    Map("male" -> Ruleset("MALE"),
+      "female" -> Ruleset("FEMALE")  ))
+
+  val Elizabethan = generator.readCulture("/elizabethan.txt",
+    Map("male" -> Ruleset("MALE" then "SURNAME"),
+      "female" -> Ruleset("FEMALE" then "SURNAME") ))
+
+  val Roman = generator.readCulture("/roman.txt",
+    Map("male" -> Ruleset("PRAENOMEN LIST" then "NOMEN LIST" then "COGNOMEN LIST"),
+      "female" -> Ruleset( feminize("NOMEN LIST") then optional("COGNOMEN LIST")) ))
+
+  val Egyptian = generator.readCulture("/fantasy_egyptian.txt",
+    Map("male" -> Ruleset("ANCIENT MALE"),
+      "female" -> Ruleset("ANCIENT FEMALE"),
+      "god" -> Ruleset("GODS"),
+      "goddess" -> Ruleset("GODDESSES")
+    ))
+
+  val Angels = generator.readCulture("/angels_and_demons.txt",
+    Map("male" -> Ruleset(suffix("MASCULINE NAMES",",") then "TITLES OF ANGELS"),
+      "female" -> Ruleset(suffix("FEMININE NAMES",",") then "TITLES OF ANGELS")
+    ))
+
+  val Russian = generator.readCulture("/russian.txt",
+    Map("male" -> Ruleset(removeBrackets("MALE") then patronymic(removeBrackets("MALE"),"ovich") then "SURNAMES"),
+      "female" -> Ruleset(removeBrackets("FEMALE") then patronymic(removeBrackets("MALE"),"ovna") then regex("SURNAMES","sky$", "skaya"))
+    ))
+
+  val Biblical = generator.readCulture("/biblical.txt",
+    Map("male" -> Ruleset(oneOf("COMMON MALE", "LESS COMMON MALE", "UNUSUAL MALE") ),
+      "female" -> Ruleset(oneOf("COMMON FEMALE", "LESS COMMON FEMALE", "UNUSUAL FEMALE") )
+    ))
 }
