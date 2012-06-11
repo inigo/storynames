b09e64fa4cd15882b6c3b2c97229158da3881947
Inigo <inigo.surguy@gmail.com>
2012-06-11 21:29:44 +0100

    Failing test for image resizing

---
+++ src/test/scala/net/surguy/android/ImageResizerTest.scala
@@ -0,0 +1,25 @@
+package net.surguy.android
+
+import android.graphics.Bitmap.CompressFormat
+import java.io.FileOutputStream
+import android.test.AndroidTestCase
+import android.graphics.BitmapFactory
+import org.junit.Test
+import junit.framework.TestCase
+
+/**
+ * @author Inigo Surguy
+ */
+class ImageResizerTest extends TestCase {
+
+  @Test
+  def testThings() {
+    val bitmap = BitmapFactory.decodeStream(this.getClass.getResourceAsStream("/res/drawable/celtic.jpg"))
+    val newBitmap = new ImageResizer().resizeImage(bitmap, 1184, 720)
+    val out = new FileOutputStream("out.jpg")
+    newBitmap.compress(CompressFormat.JPEG, 80, out)
+    out.close()
+  }
+
+
+}
